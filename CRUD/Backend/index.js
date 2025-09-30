b507151baa95b79e31a58fd3e3e04a062e34e84b
Deepesh <sainideepesh12@gmail.com>
2025-09-30 23:29:19 +0530

    update .yml file

--- CRUD/Backend/index.js
+++ CRUD/Backend/index.js
@@ -26,7 +26,7 @@ app.get("/api/user", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-  res.json({ message: "pipeline is working" });
+  res.send("pipeline is working. My name is Deepesh");
 });
 
 app.post("/api/users", async (req, res) => {
